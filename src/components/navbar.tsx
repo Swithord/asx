6e0c2421e1e42326f78267a84d8e0f175e20dd97
York Ng <yorkhayng@gmail.com>
2024-10-16 03:49:38 -0400

    collapse navbar on mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,20 +3,19 @@ import logo from '../assets/asx_logo.jpg';
 
 export default function ASXNavbar() {
     return (
-        <Navbar bg="transparent" variant="dark" className={"px-4"}>
-            {/* <Navbar.Brand href="#home">
-                <img src={logo} className={"logo"} />
-                ASX
-                </Navbar.Brand> */}
+        <Navbar bg="transparent" variant="dark" expand="lg" className={"px-4"}>
             <Navbar.Brand href="#home">
                 <img src="https://asx.sa.utoronto.ca/files/2022/07/cropped-full_white_new.png" className={"logo"} />
             </Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/events">Events</Nav.Link>
-                <Nav.Link href="/sponsor">Sponsorship</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-            </Nav>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/events">Events</Nav.Link>
+                    <Nav.Link href="/sponsor">Sponsorship</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
