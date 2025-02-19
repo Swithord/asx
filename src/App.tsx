e41fd1bb50235b0ed14efa4f19df8293e818590b
York Ng <yorkhayng@gmail.com>
2025-02-19 15:26:38 -0500

    fix analytics import?

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import AboutPage from "./pages/about";
 import EventsPage from "./pages/events";
 import ASXNavbar from "./components/navbar";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
-    <BrowserRouter>
-		<ASXNavbar />
-			<Routes>
-				<Route path="/" element={<MainPage />} />
-				<Route path="/about" element={<AboutPage />} />
-				<Route path="/events" element={<EventsPage />} />
-			</Routes>
-		<Footer />
-	</BrowserRouter>
+	<>
+		<BrowserRouter>
+			<ASXNavbar />
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+					<Route path="/about" element={<AboutPage />} />
+					<Route path="/events" element={<EventsPage />} />
+				</Routes>
+			<Footer />
+		</BrowserRouter>
+		<Analytics />
+	</>
   );
 }
 
