ba0c9bb19942a05132b3d110fe0d7ca0642cfafe
York Ng <yorkhayng@gmail.com>
2025-02-20 16:56:13 -0500

    temp changes

--- src/components/latest.tsx
+++ src/components/latest.tsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap';
 export default function Latest() {
     return (
         <Container className="latest">
-            <h1>Latest</h1>
+            <h1 className="title">Latest</h1>
         </Container>
     )
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -10,6 +10,12 @@ export default function MainPage() {
     return (
         <div className={"main"}>
             <HeroBanner />
+            <div className="flex flex-col justify-center align-center">
+                <div className='text-center'>This is a demo website.</div>
+                <div className="main-button__wrapper">
+                    <a href="https://asx.sa.utoronto.ca" target="_blank" rel="noopener noreferrer" className="main-button">Visit our Official Website</a>
+                </div>
+            </div>
             <Mission />
             <Latest />
             <Events />
